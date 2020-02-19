c75b6344dd433eb43b7356bd5204196836b0c50d
kkv <vinstark@finports.com>
2020-02-19 16:00:58 +0900

    Add: Label2

---
+++ GitTestProject/GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by finports_dev on 2020/02/19.
+//  Copyright © 2020 finports_dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
